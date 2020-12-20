93832a412dc4f02d9f645935576380e2d14038d3
Kevin F <Freymaurer@gmx.de>
2020-12-20 15:34:59 +0100

    Give design better responsiveness and add slider

---
+++ src/js/slider.js
@@ -0,0 +1,8 @@
+bulmaCarousel.attach('#carousel-demo', {
+        // https://bulma-carousel.onrender.com/#options
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 4000
+})
