e0720c74cb3f9a0285eb5b25d95953f2fc39df4d
Kevin Schneider <schneider.kev@outlook.de>
2021-07-21 11:04:46 +0200

    add template.fsx for .fsx downloads, fix ypynb link

--- src/content/tutorials_src/4_getting-started.fsx
+++ src/content/tutorials_src/4_getting-started.fsx
@@ -31,7 +31,7 @@ index: 0
 
 [![Binder]({{root}}images/badge-binder.svg)](https://mybinder.org/v2/gh/fslaborg/fslaborg.github.io/gh-pages?filepath={{fsdocs-source-basename}}.ipynb)&emsp;
 [![Script]({{root}}images/badge-script.svg)]({{root}}content/tutorials/{{fsdocs-source-basename}}.fsx)&emsp;
-[![Notebook]({{root}}images/badge-notebook.svg)]({{root}}content/tutorials/{{fsdocs-source-basename}}/.ipynb)
+[![Notebook]({{root}}images/badge-notebook.svg)]({{root}}content/tutorials/{{fsdocs-source-basename}}.ipynb)
 
 # Getting started
 
