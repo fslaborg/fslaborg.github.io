7d485872b87cf31e346b8c7507fc504cee7fa345
Heinrich Weil <hl.weil@t-online.de>
2021-10-14 19:14:32 +0200

    improve correlation network tutorial

--- src/content/tutorials_src/008_correlation-network.fsx
+++ src/content/tutorials_src/008_correlation-network.fsx
@@ -16,12 +16,19 @@ index: 1
 #r "nuget: FSharp.Stats"
 #r "nuget: Cyjs.NET"
 
+FSharp.Stats.ServiceLocator.setEnvironmentPathVariable @"C:\Users\HLWei\.nuget\packages\fsharp.stats\0.4.2\netlib_LAPACK"
+FSharp.Stats.Algebra.LinearAlgebra.Service()
+
 (***condition:ipynb***)
 #if IPYNB
 #r "nuget: FSharp.Data"
 #r "nuget: Deedle"
 #r "nuget: FSharp.Stats"
 #r "nuget: Cyjs.NET"
+
+FSharp.Stats.ServiceLocator.setEnvironmentPathVariable @"C:\Users\HLWei\.nuget\packages\fsharp.stats\0.4.2\netlib_LAPACK"
+FSharp.Stats.Algebra.LinearAlgebra.Service()
+
 #endif // IPYNB
 
 
@@ -57,6 +64,10 @@ In this tutorial, a simple workflow will be presented for how to create and visu
 #r "nuget: Cyjs.NET"
 
 do fsi.AddPrinter(fun (printer:Deedle.Internal.IFsiFormattable) -> "\n" + (printer.Format()))
+
+FSharp.Stats.ServiceLocator.setEnvironmentPathVariable @"C:\Users\USERNAME\.nuget\packages\fsharp.stats\0.4.2\netlib_LAPACK" // 
+FSharp.Stats.Algebra.LinearAlgebra.Service()
+
 ```
 
 ## Loading Data 
@@ -76,12 +87,11 @@ let rawData = Http.RequestString @"https://raw.githubusercontent.com/HLWeil/data
 // Create a deedle frame and index the rows with the values of the "Key" column.
 let rawFrame : Frame<string,string> = 
     Frame.ReadCsvString(rawData, separators = "\t")
-    |> Frame.take 1000
+    |> Frame.take 500
     |> Frame.indexRows "Key"
 
 (***hide***)
 rawFrame.Print()
-
 
 (*** include-output ***)
 
@@ -92,7 +102,7 @@ rawFrame.Print()
 Networks can be represented in many different ways. One representation which is computationally efficient in many approaches is the adjacency matrix. 
 Here every node is represented by an index and the strength of the connection between nodes is the value in the matrix at the position of their indices.
 
-In our case, the nodes of our network are genes in Escherichia coli (or bakers yeast). In a correlation network, the strength of this connection is the correlation. 
+In our case, the nodes of our network are genes in Escherichia coli (a well studied bacterium). In a correlation network, the strength of this connection is the correlation. 
 The correlation between these genes is calculated over the expression of these genes over different experiments. For this we use the pearson correlation.
 
 *)
@@ -117,18 +127,22 @@ correlationNetwork
 
 ## Critical threshold finding
 
-Thresholding correlation networks
+Creating this correlation network is not the endproduct you want though, as everything is still connected with everything. Many useful algorithms, like module finding, can only distinguish between 
+whether an edge between two vertices exists or not, instead of taking into consideration the strength of the connection. Therefore, many questions you want the network to answer, require a selection step, 
+in which strong connections are kept and weak ones are discarded. This is called thresholding. For this different algorithms exist. Here we will use an algorithm based on Random Matrix Theory (RMT). 
+
+The basic idea behind this RMT approach is filtering the network until a modular state is reached
 
 *)
 
 // Calculate the critical threshold
-// let thr,_ = Testing.RMT.compute 0.9 0.01 0.05 correlationNetwork
-let thr = 0.8
+let thr,_ = Testing.RMT.compute 0.9 0.01 0.05 correlationNetwork
 
 // Set all correlations less strong than the critical threshold to 0
 let filteredNetwork = 
     correlationNetwork
     |> Matrix.map (fun v -> if (abs v) > thr then v else 0.)
+
 
 (*** include-value:clustering ***)
 
@@ -136,7 +150,7 @@ let filteredNetwork =
 
 ## Data visualization
 
-Finally, the clustering results can be visualized to check for replicate clustering. For this we use `Cyjs.NET`, an FsLab library which makes use of the `Cytoscape.js` network visualization tool.
+Finally, the resulting network can be visualized. For this we use `Cyjs.NET`, an FsLab library which makes use of the `Cytoscape.js` network visualization tool.
 
 Further information about styling the graphs can be found [here](https://fslab.org/Cyjs.NET/).
 *)
@@ -154,7 +168,8 @@ let cytoVertices =
             Matrix.getRow filteredNetwork i 
             |> Seq.filter ((<>) 0.)
             |> Seq.length
-        let styling = [CyParam.label v; CyParam.width degree]
+            |> float
+        let styling = [CyParam.label v; CyParam.weight (sqrt degree + 1. |> (*) 10.)]
         Elements.node (string i) styling
     )
 
@@ -168,7 +183,7 @@ let cytoEdges =
                 if v <> 0. then yield i,j,v
     ]
     |> List.mapi (fun i (v1,v2,weight) -> 
-        let styling = [CyParam.weight (weight)]
+        let styling = [CyParam.weight (0.2 * weight)]
         Elements.edge ("e" + string i) (string v1) (string v2) styling
     )
 
@@ -180,12 +195,14 @@ let cytoGraph =
     |> CyGraph.withElements cytoEdges
     |> CyGraph.withStyle "node" 
         [
+            CyParam.shape "circle"
             CyParam.content =. CyParam.label
-            CyParam.width =. CyParam.width
+            CyParam.width =. CyParam.weight
+            CyParam.height =. CyParam.weight
         ]
     |> CyGraph.withStyle "edge" 
         [
-            CyParam.width =. CyParam.width
+            // CyParam.width =. CyParam.width
         ]
     |> CyGraph.withLayout (Layout.initCose (id))  
 
@@ -201,7 +218,7 @@ cytoGraph
 
 (***hide***)
 cytoGraph
-|> CyGraph.withSize(600, 400) 
+|> CyGraph.withSize(600, 900) 
 |> HTML.toEmbeddedHTML
 
 (*** include-it-raw ***)
