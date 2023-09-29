c2f2b751243e33eec53f4bc16a76edaf3f33e557
Kevin F <Freymaurer@gmx.de>
2023-09-29 23:01:09 +0200

    implement light/dark mode

--- src/Components.fs
+++ src/Components.fs
@@ -2,6 +2,49 @@ namespace App
 
 open Feliz
 open Feliz.Router
+open Feliz.Bulma
+
+module ThemeSwitch =
+
+    let getDataTheme() =
+        Browser.Dom.document.documentElement.getAttribute("data-theme")
+
+    type DataTheme =
+    | Dark 
+    | Light
+        static member ofString (str: string) =
+            if isNull str then Light
+            else
+                match str.ToLower() with
+                | "dark" -> Dark
+                | "light" | _ -> Light
+        member this.setOppositeTheme(setter: DataTheme -> unit) =
+            let prev = getDataTheme()
+            Browser.Dom.console.log(prev)
+            let next = if this = Dark then Light else Dark
+            Browser.Dom.document.documentElement.setAttribute("data-theme", (string next).ToLower() )
+            setter next
+
+        member this.isDark = this = Dark
+
+    type State = {
+        Theme: DataTheme
+    } with
+        static member init() = 
+            let dt = getDataTheme()
+            Browser.Dom.console.log("[DATA-THEME]", dt)
+            { Theme = DataTheme.ofString dt }
+
+    let button() = 
+        let state, update = React.useState(State.init)
+        Bulma.button.button [
+            let t = match state.Theme with | Dark -> "dark" | Light -> "light"
+            prop.text t
+            prop.onClick <| fun _ ->
+                let setter = fun (theme: DataTheme) ->
+                    update {state with Theme = theme}
+                state.Theme.setOppositeTheme(setter)
+        ]
 
 type Components =
     /// <summary>
@@ -23,6 +66,7 @@ type Components =
                 prop.onClick (fun _ -> setCount(count + 1))
                 prop.text "Increment"
             ]
+            ThemeSwitch.button()
         ]
 
     /// <summary>
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   root: "./src",
   build: {
-    outDir: "../dist",
+    outDir: "../docs",
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ['node_modules']
+      }
+    }
   }
 })
